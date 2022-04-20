4045f1611ee18bccf4667f5377e71f5b8ec398cb
Aleksei Chamin <aleksei_chamin@epam.com>
2022-04-20 23:22:14 +0300

    Refactoring

--- CommandsService/Startup.cs
+++ CommandsService/Startup.cs
@@ -15,8 +15,7 @@ namespace CommandsService
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -26,8 +25,7 @@ namespace CommandsService
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CommandsService", Version = "v1" });
             });
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
